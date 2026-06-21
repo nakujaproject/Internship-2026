714384cf23b32f537685a441f0a9e7ec61734f96
Ehenzo <124528808+lituehenzo@users.noreply.github.com>
2026-06-21 12:27:34 -0700

    Update trigger board pin mapping
    Removed comment about updating pin assignments.

--- 01_SOLID_PROPULSION/03_Ignition_Circuit/Software/Trigger_Board_Firmware/pins.h
+++ 01_SOLID_PROPULSION/03_Ignition_Circuit/Software/Trigger_Board_Firmware/pins.h
@@ -2,12 +2,8 @@
 #define PINS_H
 
 // Trigger Board Pin Mapping
-// Update this file whenever the PCB, wiring, or firmware pin assignment changes.
 
-// Operator feedback
 #define BUZZER_PIN 18
-
-// Operator input
 #define STATUS_BUTTON_PIN 32
 
 #endif
